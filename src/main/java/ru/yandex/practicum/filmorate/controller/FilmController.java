9197325dc82cae02357fabbc2fb1e2f9cedae61b
yalozok <olga.kozmovskaia@yandex.ru>
2025-03-07 13:06:45 +0100

    Add Film and FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,55 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.groups.Default;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Validator;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
+@Validated
+@Slf4j
 public class FilmController {
+    Long id = 0L;
+    Map<Long, Film> films = new HashMap<>();
+
+    private Long getNextId() {
+        return ++id;
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        log.info("Get films: {} - Started and Finished", films.size());
+        return films.values();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film createFilm(@RequestBody @Validated({Default.class, Validator.Create.class}) Film film) {
+        log.info("Create film: {} - Started", film);
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Create film: {} - Finished", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateUser(@RequestBody @Validated({Default.class, Validator.Update.class}) Film film) {
+        log.info("Update film: {} - Started", film);
+        if (!films.containsKey(film.getId())) {
+            log.error("Фильм с id {} не найден", film.getId());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Фильм с id: " + film.getId() + " не найден");
+        }
+        films.put(film.getId(), film);
+        log.info("Update film: {} - Finished", film);
+        return film;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,32 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1985, 12, 28);
+    @Null(groups = Validator.Create.class)
+    @NotNull(groups = Validator.Update.class)
+    private Long id;
+
+    @NotBlank(message = "Название фильма не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "Описание фильма не должно превышать 200 символов")
+    private String description;
+
+    @NotNull(message = "Дата релиза обязательна")
+    @Past(message = "Дата релиза не может быть в будущем")
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительной")
+    private int duration;
+
+    @AssertTrue(message = "Дата редиза не может быть раньше 28 декабря 1895")
+    private boolean isValidReleaseDate() {
+        return releaseDate.isAfter(CINEMA_BIRTHDAY);
+    }
 }
