0e3e5c0c8865dc892224ebe0ef1e17222bf4e286
yalozok <olga.kozmovskaia@yandex.ru>
2025-04-15 22:07:44 +0200

    Update User Service

--- src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserServiceImpl.java
@@ -5,87 +5,86 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
+import ru.yandex.practicum.filmorate.repository.FriendRepository;
+import ru.yandex.practicum.filmorate.repository.UserRepository;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
-    private final UserStorage userStorage;
+    private final UserRepository userRepository;
+    private final FriendRepository friendRepository;
 
     @Override
     public List<User> getAll() {
-        return userStorage.getAll();
+        return userRepository.getAll();
     }
 
     @Override
     public User get(long id) {
-        return userStorage.get(id)
+        return userRepository.getById(id)
                 .orElseThrow(() -> new NotFoundException("User was not found with id: " + id));
     }
 
     @Override
     public User add(User user) {
-        if (userStorage.isDuplicateEmail(user.getEmail())) {
+        if (userRepository.isDuplicateEmail(user.getEmail())) {
             throw new ValidationException("Email is already in use " + user.getEmail());
         }
-        return userStorage.add(user);
+        return userRepository.add(user);
     }
 
     @Override
     public User update(User user) {
-        User oldUser = userStorage.get(user.getId())
+        User oldUser = userRepository.getById(user.getId())
                 .orElseThrow(() -> new NotFoundException("User was not found with id: " + user.getId()));
 
-        if (!oldUser.getEmail().equals(user.getEmail()) && userStorage.isDuplicateEmail(user.getEmail())) {
+        if (!oldUser.getEmail().equals(user.getEmail()) && userRepository.isDuplicateEmail(user.getEmail())) {
             throw new ValidationException("Email is already in use " + user.getEmail());
         }
 
         if (user.getBirthday() == null) {
             user.setBirthday(oldUser.getBirthday());
         }
-        return userStorage.update(user);
+        return userRepository.update(user);
     }
 
     @Override
     public void delete(long id) {
-        userStorage.get(id).orElseThrow(() -> new NotFoundException("User was not found with id: " + id));
-        userStorage.delete(id);
+        userRepository.getById(id).orElseThrow(() -> new NotFoundException("User was not found with id: " + id));
+        userRepository.delete(id);
     }
 
     @Override
     public void addFriend(long userId, long friendId) {
-        userStorage.get(userId)
+        userRepository.getById(userId)
                 .orElseThrow(() -> new NotFoundException("User was not found with id: " + userId));
-        userStorage.get(friendId)
+        userRepository.getById(friendId)
                 .orElseThrow(() -> new NotFoundException("User as friend was not found with id: " + friendId));
-        userStorage.addFriend(userId, friendId);
+        friendRepository.addFriend(userId, friendId);
     }
 
+    @Override
     public void removeFriend(long userId, long friendId) {
-        userStorage.get(userId)
+        userRepository.getById(userId)
                 .orElseThrow(() -> new NotFoundException("User was not found with id: " + userId));
-        userStorage.get(friendId)
+        userRepository.getById(friendId)
                 .orElseThrow(() -> new NotFoundException("User as friend was not found with id: " + friendId));
-        userStorage.removeFriend(userId, friendId);
+        friendRepository.removeFriend(userId, friendId);
     }
 
+    @Override
     public List<User> getFriends(long userId) {
-        userStorage.get(userId).orElseThrow(() -> new NotFoundException("User was not found with id: " + userId));
-        return userStorage.getFriends(userId)
-                .orElseGet(Collections::emptyList);
+        userRepository.getById(userId).orElseThrow(() -> new NotFoundException("User was not found with id: " + userId));
+        return friendRepository.getFriends(userId);
     }
 
+    @Override
     public List<User> getCommonFriends(long userId, long otherId) {
-        List<User> userFriends = userStorage.getFriends(userId)
-                .orElseThrow(() -> new NotFoundException("User was not found with id: " + userId));
-
-        List<User> friendFriends = userStorage.getFriends(otherId)
-                .orElseThrow(() -> new NotFoundException("User as friend was not found with id: " + otherId));
-
+        List<User> userFriends = friendRepository.getFriends(userId);
+        List<User> friendFriends = friendRepository.getFriends(otherId);
         return userFriends.stream().filter(friendFriends::contains).collect(Collectors.toList());
     }
 }
